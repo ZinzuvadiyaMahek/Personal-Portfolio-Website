9634fb3d5c21b8bc7dd2e437ff5fb3141f3833dc
ZinzuvadiyaMahek <zinzuvadiya.mahek.08@gmail.com>
2026-07-09 20:13:11 +0530

    Update GitHub link for personal portfolio website

--- project.js
+++ project.js
@@ -6,7 +6,7 @@ const projectList = [
                 tech: ["HTML5","CSS3","JavaScript","Responsive Design"],
                 details: "A modern and responsive personal portfolio website developed to showcase my skills, projects, and educational background. The website features structured sections, dynamic content rendering using JavaScript, and a clean user interface to provide smooth navigation. It highlights my development abilities while presenting my work and achievements in a professional and visually appealing way.",
                 live: "http://127.0.0.1:5500/index.html",
-                github: "https://github.com/MahekKZ/Personal-Portfolio-Website",
+                github: "https://github.com/ZinzuvadiyaMahek/Personal-Portfolio-Website",
             } , 
             {
                 id:2,
